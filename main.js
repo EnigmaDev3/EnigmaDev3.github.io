8b266b35682556ce5662b44c7d9cddf8e383f248
AnastasIA <108724265+EnigmaDev3@users.noreply.github.com>
2023-09-27 14:04:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 // Эта функция будет вызываться при клике на кнопку или другое событие и будет переключать видимость блока с информацией и текст кнопки
 
 function showMoreInfo() {
-    const moreInfo = document.getElementById('more-info');
-    const button = document.querySelector('.btn-box');
-
+    const moreInfo = document.getElementById('more-info');  // Получаем элемент с информацией по его идентификатору 'more-info'
+    const button = document.querySelector('.btn-box');  // Получаем элемент кнопки с классом 'btn-box'
+ // Если блок скрыт (display: none), то делаем его видимым и меняем текст кнопки на 'hide'
     if (moreInfo.style.display === 'none') {
         moreInfo.style.display = 'block';
         button.textContent = 'hide';
+// Если блок видим, то скрываем его (display: none) и меняем текст кнопки на 'more about me'
     } else {
         moreInfo.style.display = 'none';
         button.textContent = 'more about me';
@@ -19,9 +20,9 @@ function showMoreInfo() {
 
     var typed = new Typed(".text", {
         strings: ["Anastasia"],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true
+        typeSpeed: 100,  // Скорость печати текста
+        backSpeed: 100,  // Скорость удаления текста (если есть)
+        backDelay: 1000, // Задержка перед началом удаления текста
+        loop: true // Зацикливаем анимацию
     });
     
